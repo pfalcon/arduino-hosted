c615bb20376b23a1fc7990c798b300af16e70b99
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2012-04-26 00:29:18 +0300

    Move to class-based implementation, to support multiple connected boards.

--- arduino.py
+++ arduino.py
@@ -18,24 +18,6 @@ LED = 0
 BUTTON = 3
 
 
-def delay(miliseconds):
-    time.sleep(float(miliseconds) / 1000)
-
-def pinMode(pin, mode):
-    cmd = "pinmode p%d %s" % (pin, ["in", "out"][mode])
-    bus.command(cmd)
-
-def digitalWrite(pin, val):
-#    cmd = "p%s.%s=%s" % (pin / 8 + 1, pin % 8, val)
-    cmd = "p%d=%d" % (pin, val)
-    bus.command(cmd)
-
-def digitalRead(pin):
-    cmd = "p%s.%s?" % (pin / 8 + 1, pin % 8)
-    bus.command(cmd)
-    resp = bus.get_response()
-    assert resp.startswith("READ: "), resp
-    return int(resp[-1])
 
 
 class SPIClass:
@@ -67,9 +49,106 @@ class SPIClass:
         return int(read[len("READ: "):], 16)
 
 
-port = None
-bus = None
-SPI = None
+class Arduino:
+
+    def __init__(self, port=None, board=None, debug=False):
+        self.board_config = None
+        self.port = port
+        self.board_type = None
+        if self.port is None:
+            if board:
+                port_spec = self.get_board_config(board)
+            elif "BUSNINJA_PORT" in os.environ:
+                port_spec  = os.environ["BUSNINJA_PORT"]
+            else:
+                port_spec = self.detect_board()
+
+            self.board_type, serial_dev, baud = self.parse_port_spec(port_spec)
+            self.port = serial.Serial(serial_dev, baud, timeout=0.3)
+
+        if self.board_type is None:
+            self.board_type = "arduino"
+
+        soft_uart = False
+        if self.board_type == "arduino":
+            self.LED = 13
+        else:
+            soft_uart = True
+            self.LED = 0
+
+        self.bus = BusPirate(self.port, soft_uart=soft_uart, debug=debug)
+        self.bus.connect()
+        self.SPI = SPIClass(self.bus)
+
+
+    def parse_port_spec(self, port_spec):
+        arr = port_spec.split(":")
+        dev_type = None
+        baud = 9600
+        if len(arr) == 1:
+            port_str = arr[0]
+        elif len(arr) == 2:
+            try:
+                baud = int(arr[1])
+                port_str = arr[0]
+            except ValueError:
+                board_type = arr[0]
+                port_str = arr[1]
+        elif len(arr) == 3:
+                board_type = arr[0]
+                port_str = arr[1]
+                baud = int(arr[2])
+        else:
+            raise ValueError("Invalid syntax for BUSNINJA_PORT: expected [<device_type>:]/dev/<serial>[:<baud>]")
+        return board_type, port_str, baud
+
+    def get_board_config(self, board):
+        if self.board_config is None:
+            self.board_config = {}
+            f = open("board.cfg")
+            for l in f:
+                k, v = [s.strip() for s in l.split("=", 1)]
+                self.board_config[k] = v
+        return self.board_config.get(board)
+
+
+    def run(self, app):
+        try:
+            app.setup()
+            while True:
+                app.loop()
+        except:
+            self.port.read(100)
+            raise
+
+    def run_func(self, func):
+        try:
+            func()
+        except:
+            self.port.read(100)
+            raise
+
+
+    def delay(self, miliseconds):
+        time.sleep(float(miliseconds) / 1000)
+
+    def pinMode(self, pin, mode):
+        cmd = "pinmode p%d %s" % (pin, ["in", "out"][mode])
+        self.bus.command(cmd)
+
+    def digitalWrite(self, pin, val):
+    #    cmd = "p%s.%s=%s" % (pin / 8 + 1, pin % 8, val)
+        cmd = "p%d=%d" % (pin, val)
+        self.bus.command(cmd)
+
+    def digitalRead(self, pin):
+        cmd = "p%s.%s?" % (pin / 8 + 1, pin % 8)
+        self.bus.command(cmd)
+        resp = self.bus.get_response()
+        assert resp.startswith("READ: "), resp
+        return int(resp[-1])
+
+
 
 def get_port():
     global port
